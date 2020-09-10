a6a9256968a03d67924c94e3752b2937dc69cc6d
javierelpianista <javier.garcia.tw@hotmail.com>
2020-09-10 16:58:56 -0300

    Refactored create_profile.py; now it defines a class CreateProfileWindow, and all the functions are now methods of this class.

--- create_profile.py
+++ create_profile.py
@@ -7,238 +7,298 @@ import run_command
 from variables import wad_dirs, default_config_filename, main_dir, profile_dir
 from profile import Profile
 
-# Get the list of available wad files
-wad_list, iwad_list = run_command.get_list_of_wad_files(wad_dirs)
+class CreateProfileWindow(tk.Toplevel):
+    def cancel(self):
+        self.destroy()
 
-# These lists will contain the selected IWAD and WADs
-selected_wads_list  = []
-selected_iwads_list = []
+    def get_listbox_selection(self, val):
+        sender = val.widget
+        inds   = sender.curselection()
 
-os.makedirs(main_dir, exist_ok = True)
+        #values.clear()
+        for idx in inds:
+            values.append(sender.get(idx))
 
-def create_profile():
-    # If the user let the default, we have to generate a ListboxSelect event to use the default
-    # value.
-    if not selected_iwads_list:
-        iwads_listbox.event_generate('<<ListboxSelect>>')
+        return(values)
 
-    profile_name = profile_entry.get()
+    def select_profile_name(self):
+        profile_name = filedialog.asksaveasfilename(
+                defaultextension = '.gzd',
+                filetypes        = [('py_gzdoom_launcher profile file', '.gzd')],
+                initialdir       = profile_dir,
+                title            = 'Profile file'
+                )
+        self.profile_entry.delete(0, tk.END)
+        self.profile_entry.insert(0, profile_name)
+
+    def select_config_filename(self):
+        config_filename   = filedialog.asksaveasfilename()
+        self.config_file_entry.delete(0, tk.END)
+        self.config_file_entry.insert(0, config_filename)
+
+    def create_profile(self):
+        selected_iwad = self.iwads_listbox.get(self.iwads_listbox.curselection())
+        selected_wads_list = [self.wads_listbox.get(x) for x in self.wads_listbox.curselection()]
+        profile_name = self.profile_entry.get()
+        config_file = self.config_file_entry.get()
+        
+        profile = Profile(
+                wads = selected_wads_list,
+                iwad = selected_iwad,
+                config_file = config_file,
+                dmflags = 0,
+                dmflags2 = 0
+                )
+
+        profile_filename = os.path.join(profile_dir, profile_name + '.gzd')
+
+        if os.path.exists(profile_filename):
+            messagebox.showerror(
+                    title   = 'Profile exists', 
+                    message = 'ERROR! Profile {} already exists.'.format(profile_name))
+        else:
+            print('Generating profile: {}'.format(profile_name))
+            print(profile.get_info())
+            profile.save(profile_filename)
+            messagebox.showinfo(
+                    title    = 'Profile created', 
+                    message  = 'Profile {} created successfully'.format(profile_name)
+                    )
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        #self = tk.Toplevel(parent)
+        self.title('Profile generator')
+        self.attributes('-type', 'dialog')
+        self.grab_set()
     
-    profile = Profile(
-            wads = selected_wads_list,
-            iwad = selected_iwads_list[0],
-            config_file = config_file_entry.get(),
-            dmflags = 0,
-            dmflags2 = 0
-            )
+        ########################################
+        # This frame contains the profile name #
+        ########################################
+        self.frame_profile = ttk.Frame(
+                master = self
+                )
 
-    print('Generating profile: {}'.format(profile_name))
-    print('Profile directory: {}'.format(profile_dir))
-    print(profile.get_info())
+        self.profile_label = ttk.Label(
+                master = self.frame_profile,
+                text   = 'Profile name'
+                )
 
-    profile_filename = os.path.join(profile_dir, profile_name + '.gzd')
-    if os.path.exists(profile_filename):
-        messagebox.showerror(title = 'Profile exists', message = 'ERROR! Profile {} already exists.'.format(profile_name))
-    else:
-        profile.save(profile_filename)
+        self.profile_entry = ttk.Entry(
+                master = self.frame_profile,
+                )
 
-def handle_exit(event):
-    exit()
+        self.profile_label.pack()
+        self.profile_entry.pack(fill = tk.X, expand = True, padx = 5)
 
-def exit():
-    quit()
+        self.frame_profile.pack(fill = tk.X, pady = 5)
 
-# Given a listbox, put all the selected items into the values list
-def get_listbox_selection(val, values):
-    sender = val.widget
-    inds   = sender.curselection()
+        #####################################################################
+        # The following frame will contain two horizontally located frames. #
+        # One for IWADs and another one for WADs.                           #
+        #####################################################################
+        self.frame_wads = ttk.Frame(
+                master = self, 
+                )
+ 
+        # This frame occupies the left portion of the top frame
+        # and contains a listbox with the available IWADs
+        self.frame_wads_iwads = tk.Frame(
+                master = self.frame_wads,
+                )
+ 
+        self.frame_wads_iwads.pack(
+                fill = tk.BOTH,
+                expand = True,
+                side = tk.LEFT,
+                padx = 5
+                )
+ 
+        # Label for the IWAD radiobuttons
+        self.label_iwad = ttk.Label(
+                master = self.frame_wads_iwads,
+                text   = "Choose the IWAD",
+                )
+ 
+        self.label_iwad.pack()
+ 
+        # Listbox containing the IWADS
+        self.iwads_listbox = tk.Listbox(
+                master          = self.frame_wads_iwads, 
+                selectmode      = tk.BROWSE, 
+                exportselection = False
+                )
+ 
+        ## Get the list of available wad files
+        wad_list, iwad_list = run_command.get_list_of_wad_files(wad_dirs)
 
-    values.clear()
-    for idx in inds:
-        values.append(sender.get(idx))
+        for n, iwad in enumerate(iwad_list):
+            self.iwads_listbox.insert(tk.END, iwad) 
+ 
+        self.iwads_listbox.select_set(0)
+        self.iwads_listbox.pack(expand = True, fill = tk.BOTH, padx = 5, pady = (0, 5))
 
-# This opens a dialog to select the configuration file
-def config_file_browser():
-    global config_file_entry
+        # Here is the frame with available WADs
+        self.frame_wads_wads = tk.Frame(
+                master = self.frame_wads,
+                )
 
-    config_filename   = filedialog.asksaveasfilename()
-    config_file_entry.delete(0, tk.END)
-    config_file_entry.insert(0, config_filename)
+        self.frame_wads_wads.pack(
+                fill = tk.BOTH,
+                expand = True,
+                side = tk.RIGHT,
+                padx = 5
+                )
 
-# Open a dialog to select the profile file
-def profile_file_browser():
-    global profile_entry
+        self.wads_listbox = tk.Listbox(
+                master          = self.frame_wads_wads,
+                selectmode      = tk.MULTIPLE,
+                exportselection = False
+                )
 
-    profile_name = filedialog.asksaveasfilename(defaultextension = '.gzd')
-    profile_entry.delete(0, tk.END)
-    profile_entry.insert(0, profile_name)
+        for n, wad in enumerate(wad_list):
+            self.wads_listbox.insert(tk.END, wad)
 
-window = tk.Tk()
-window.title('Profile generator')
-window.attributes('-type', 'dialog')
-window.bind('<Control-q>', handle_exit)
+        self.label_wad = ttk.Label(
+                master = self.frame_wads_wads,
+                text   = "Available WADs",
+                )
 
-window.style = ttk.Style()
-window.style.theme_use('default')
+        self.label_wad.pack()
+        self.wads_listbox.pack(expand = True, fill = tk.BOTH, padx = 5, pady = (0, 5))
 
-# This frame contains the options for WADs and IWADs.
-frame_wads = ttk.Frame(
-        master = window, 
-        )
 
-# This frame occupies the left portion of the top frame
-# and contains a listbox with the available IWADs
-frame_wads_iwads = tk.Frame(
-        master = frame_wads,
-        )
+        self.frame_wads.pack(pady = 5, fill = tk.BOTH, expand = True)
 
-frame_wads_iwads.pack(
-        fill = tk.BOTH,
-        expand = True,
-        side = tk.LEFT,
-        padx = 5
-        #expand = True,
-        )
+        ###############################################################################
+        # This frame lets the user choose the configuration file bound to the profile #
+        ###############################################################################
+        self.frame_config = tk.Frame(
+                master = self,
+                )
+        self.config_label = ttk.Label(
+                master = self.frame_config,
+                text   = 'Configuration file'
+                )
+    
+        self.config_file_entry = ttk.Entry(
+                master = self.frame_config,
+                )
+    
+        self.config_file_entry.insert(0, default_config_filename)
+    
+        self.browse_config_button = ttk.Button(
+                master  = self.frame_config,
+                text    = 'Browse...',
+                command = self.select_config_filename
+                )
+    
+        self.config_label.pack()
+        self.config_file_entry.pack(side = tk.LEFT, fill = tk.X, expand = True, padx = 5)
+        self.browse_config_button.pack(side = tk.RIGHT, padx = 5)
 
-# Label for the IWAD radiobuttons
-label_iwad = ttk.Label(
-        master = frame_wads_iwads,
-        text   = "Choose the IWAD",
-        )
+        self.frame_config.pack(fill = tk.X, pady = 5)
+    
+        #############################################################
+        # This frame contains the create profile and cancel buttons #
+        #############################################################
+        self.frame_buttons = ttk.Frame(
+                master = self,
+                )
 
-label_iwad.pack()
+        self.profile_button = ttk.Button(
+                master  = self.frame_buttons,
+                text    = 'Create profile', 
+                command = self.create_profile,
+                )
 
-# Listbox containing the IWADS
-iwads_listbox = tk.Listbox(
-        master          = frame_wads_iwads, 
-        selectmode      = tk.BROWSE, 
-        exportselection = False
-        )
+        self.profile_button.pack(side = tk.LEFT, expand = True, fill = tk.X, padx = 5)
 
-for n, iwad in enumerate(iwad_list):
-    iwads_listbox.insert(tk.END, iwad) 
+        self.cancel_button = ttk.Button(
+                master  = self.frame_buttons,
+                text    = "Cancel",
+                command = lambda: self.cancel()
+                )
 
-iwads_listbox.bind('<<ListboxSelect>>', lambda x: get_listbox_selection(x, selected_iwads_list))
-iwads_listbox.select_set(0)
-iwads_listbox.pack(expand = True, fill = tk.BOTH, padx = 5, pady = (0, 5))
+        self.cancel_button.pack(side = tk.RIGHT, expand = True, fill = tk.X, padx = 5)
+        self.frame_buttons.pack(fill = tk.X, pady = 5)
 
-########################################
-# This frame contains the profile name #
-########################################
-frame_profile = ttk.Frame(
-        master = window
-        )
-
-profile_label = ttk.Label(
-        master = frame_profile,
-        text   = 'Profile file'
-        )
-
-profile_entry = ttk.Entry(
-        master = frame_profile,
-        )
-
-browse_button = ttk.Button(
-        master  = frame_profile,
-        text    = 'Browse...',
-        command = profile_file_browser
-        )
-browse_button.pack(side = tk.RIGHT, padx = 5)
-
-profile_label.pack()
-profile_entry.insert(0, os.path.join(profile_dir, 'default.gzd'))
-profile_entry.pack(side = tk.LEFT, fill = tk.X, expand = True, padx = 5)
-
-#########################################################
-# This frame occupies the left portion of the top frame #
-# and contains a listbox with the available WADs        #
-#########################################################
-
-frame_wads_wads = tk.Frame(
-        master = frame_wads,
-        )
-
-frame_wads_wads.pack(
-        fill = tk.BOTH,
-        expand = True,
-        side = tk.RIGHT,
-        padx = 5
-        )
-
-wads_listbox = tk.Listbox(
-        master          = frame_wads_wads,
-        selectmode      = tk.MULTIPLE,
-        exportselection = False
-        )
-
-for n, wad in enumerate(wad_list):
-    wads_listbox.insert(tk.END, wad)
-
-label_wad = ttk.Label(
-        master = frame_wads_wads,
-        text   = "Available WADs",
-        )
-
-label_wad.pack()
-wads_listbox.bind('<<ListboxSelect>>', lambda x: get_listbox_selection(x, selected_wads_list))
-wads_listbox.pack(expand = True, fill = tk.BOTH, padx = 5, pady = (0, 5))
-
-##############################################
-# Options related to the configuration files #
-##############################################
-
-frame_config = tk.Frame(
-        master = window,
-        )
-config_label = ttk.Label(
-        master = frame_config,
-        text   = 'Configuration file'
-        )
-
-config_file_entry = ttk.Entry(
-        master = frame_config,
-        )
-
-config_file_entry.insert(0, default_config_filename)
-
-browse_button = ttk.Button(
-        master  = frame_config,
-        text    = 'Browse...',
-        command = config_file_browser
-        )
-
-config_label.pack()
-config_file_entry.pack(side = tk.LEFT, fill = tk.X, expand = True, padx = 5)
-browse_button.pack(side = tk.RIGHT, padx = 5)
-
-##################################################
-# These frames contain the buttons at the bottom #
-##################################################
-
-frame_buttons = ttk.Frame(
-        master = window,
-        )
-
-profile_button = ttk.Button(
-        master  = frame_buttons,
-        text    = 'Create profile', 
-        command = create_profile,
-        )
-
-profile_button.pack(side = tk.LEFT, expand = True, fill = tk.X, padx = 5)
-
-exit_button = ttk.Button(
-        master  = frame_buttons,
-        text    = "Exit",
-        command = exit
-        )
-
-exit_button.pack(side = tk.RIGHT, expand = True, fill = tk.X, padx = 5)
-
-frame_profile.pack(fill = tk.X, pady = 5)
-frame_wads.pack(pady = 5, fill = tk.BOTH, expand = True
-    )
-frame_config.pack(fill = tk.X, pady = 5)
-frame_buttons.pack(fill = tk.X, pady = 5)
-
-tk.mainloop()
+#
+## These lists will contain the selected IWAD and WADs
+#
+#os.makedirs(main_dir, exist_ok = True)
+#
+#def cancel(window):
+#    window.destroy()
+#
+## Given a listbox, put all the selected items into the values list
+#
+## Open a dialog to select the profile file
+#def profile_file_browser():
+#    global profile_entry
+#
+#
+#def create_profile_window(root):
+#    ########################################
+#    # This frame contains the profile name #
+#    ########################################
+#    frame_profile = ttk.Frame(
+#            master = window
+#            )
+#
+#    profile_label = ttk.Label(
+#            master = frame_profile,
+#            text   = 'Profile file'
+#            )
+#
+#    profile_entry = ttk.Entry(
+#            master = frame_profile,
+#            )
+#
+#    browse_button = ttk.Button(
+#            master  = frame_profile,
+#            text    = 'Browse...',
+#            command = profile_file_browser
+#            )
+#    browse_button.pack(side = tk.RIGHT, padx = 5)
+#
+#    profile_label.pack()
+#    profile_entry.insert(0, os.path.join(profile_dir, 'default.gzd'))
+#    profile_entry.pack(side = tk.LEFT, fill = tk.X, expand = True, padx = 5)
+#
+#    #########################################################
+#    # This frame occupies the left portion of the top frame #
+#    # and contains a listbox with the available WADs        #
+#    #########################################################
+#
+#    ##################################################
+#    # These frames contain the buttons at the bottom #
+#    ##################################################
+#
+#    frame_buttons = ttk.Frame(
+#            master = window,
+#            )
+#
+#    profile_button = ttk.Button(
+#            master  = frame_buttons,
+#            text    = 'Create profile', 
+#            command = create_profile,
+#            )
+#
+#    profile_button.pack(side = tk.LEFT, expand = True, fill = tk.X, padx = 5)
+#
+#    cancel_button = ttk.Button(
+#            master  = frame_buttons,
+#            text    = "Cancel",
+#            command = lambda: cancel(window)
+#            )
+#
+#    cancel_button.pack(side = tk.RIGHT, expand = True, fill = tk.X, padx = 5)
+#
+#    frame_profile.pack(fill = tk.X, pady = 5)
+#    frame_wads.pack(pady = 5, fill = tk.BOTH, expand = True
+#        )
+#
+#    return window
+#
--- variables.py
+++ variables.py
@@ -1,3 +1,10 @@
+import os
+import os.path
+
+main_dir = os.path.join(os.path.expanduser('~'), '.py_gzdoom_launcher')
+profile_dir = main_dir
 wad_dirs = '/usr/share/games/doom'
+
 default_config_filename = '/home/jgarcia/py_gzdoom_launcher/gzdoom.cfg'
-iwad_names = ['doom', 'doom2', 'plutonia', 'tnt', 'heretic', 'hexen']
+
+iwad_names = ['doom', 'doom2', 'plutonia', 'tnt', 'heretic', 'hexen', 'harmony', 'hedon']
