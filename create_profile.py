c7da1771623179faa0b7d7b128b06651232617ae
javierelpianista <javier.garcia.tw@hotmail.com>
2020-09-10 13:34:20 -0300

    Changed main.py into create_profile.py. This window is used to create a profile from scratch. Now main.py will be a window that is used to either select a profile, create one or edit one.

--- create_profile.py
+++ create_profile.py
@@ -1,9 +1,11 @@
+#!/bin/python
+
 import tkinter as tk
-from tkinter import ttk 
-from tkinter import filedialog
+from tkinter import ttk, filedialog, messagebox
 import os
 import run_command
-from variables import wad_dirs, default_config_filename
+from variables import wad_dirs, default_config_filename, main_dir, profile_dir
+from profile import Profile
 
 # Get the list of available wad files
 wad_list, iwad_list = run_command.get_list_of_wad_files(wad_dirs)
@@ -12,27 +14,33 @@ wad_list, iwad_list = run_command.get_list_of_wad_files(wad_dirs)
 selected_wads_list  = []
 selected_iwads_list = []
 
-def run(window):
+os.makedirs(main_dir, exist_ok = True)
+
+def create_profile():
     # If the user let the default, we have to generate a ListboxSelect event to use the default
     # value.
     if not selected_iwads_list:
         iwads_listbox.event_generate('<<ListboxSelect>>')
 
-    command = 'gzdoom' 
-    for wad in selected_wads_list:
-        command += ' -file {}'.format(wad)
+    profile_name = profile_entry.get()
+    
+    profile = Profile(
+            wads = selected_wads_list,
+            iwad = selected_iwads_list[0],
+            config_file = config_file_entry.get(),
+            dmflags = 0,
+            dmflags2 = 0
+            )
 
-    iwad_name = selected_iwads_list[0]
-    command += ' -iwad {}'.format(iwad_name)
+    print('Generating profile: {}'.format(profile_name))
+    print('Profile directory: {}'.format(profile_dir))
+    print(profile.get_info())
 
-    config_filename = config_file_entry.get()
-    command += ' -config {}'.format(config_filename)
-
-    window.destroy()
-
-    print('Running gzdoom with the following command...')
-    print(command)
-    os.system(command)
+    profile_filename = os.path.join(profile_dir, profile_name + '.gzd')
+    if os.path.exists(profile_filename):
+        messagebox.showerror(title = 'Profile exists', message = 'ERROR! Profile {} already exists.'.format(profile_name))
+    else:
+        profile.save(profile_filename)
 
 def handle_exit(event):
     exit()
@@ -49,8 +57,25 @@ def get_listbox_selection(val, values):
     for idx in inds:
         values.append(sender.get(idx))
 
+# This opens a dialog to select the configuration file
+def config_file_browser():
+    global config_file_entry
+
+    config_filename   = filedialog.asksaveasfilename()
+    config_file_entry.delete(0, tk.END)
+    config_file_entry.insert(0, config_filename)
+
+# Open a dialog to select the profile file
+def profile_file_browser():
+    global profile_entry
+
+    profile_name = filedialog.asksaveasfilename(defaultextension = '.gzd')
+    profile_entry.delete(0, tk.END)
+    profile_entry.insert(0, profile_name)
+
 window = tk.Tk()
-window.title('GZDoom launcher')
+window.title('Profile generator')
+window.attributes('-type', 'dialog')
 window.bind('<Control-q>', handle_exit)
 
 window.style = ttk.Style()
@@ -97,8 +122,38 @@ iwads_listbox.bind('<<ListboxSelect>>', lambda x: get_listbox_selection(x, selec
 iwads_listbox.select_set(0)
 iwads_listbox.pack(expand = True, fill = tk.BOTH, padx = 5, pady = (0, 5))
 
-# This frame occupies the left portion of the top frame
-# and contains a listbox with the available WADs
+########################################
+# This frame contains the profile name #
+########################################
+frame_profile = ttk.Frame(
+        master = window
+        )
+
+profile_label = ttk.Label(
+        master = frame_profile,
+        text   = 'Profile file'
+        )
+
+profile_entry = ttk.Entry(
+        master = frame_profile,
+        )
+
+browse_button = ttk.Button(
+        master  = frame_profile,
+        text    = 'Browse...',
+        command = profile_file_browser
+        )
+browse_button.pack(side = tk.RIGHT, padx = 5)
+
+profile_label.pack()
+profile_entry.insert(0, os.path.join(profile_dir, 'default.gzd'))
+profile_entry.pack(side = tk.LEFT, fill = tk.X, expand = True, padx = 5)
+
+#########################################################
+# This frame occupies the left portion of the top frame #
+# and contains a listbox with the available WADs        #
+#########################################################
+
 frame_wads_wads = tk.Frame(
         master = frame_wads,
         )
@@ -140,13 +195,6 @@ config_label = ttk.Label(
         text   = 'Configuration file'
         )
 
-def config_file_browser():
-    global config_file_entry
-
-    config_filename   = filedialog.asksaveasfilename()
-    config_file_entry.delete(0, tk.END)
-    config_file_entry.insert(0, config_filename)
-
 config_file_entry = ttk.Entry(
         master = frame_config,
         )
@@ -171,13 +219,13 @@ frame_buttons = ttk.Frame(
         master = window,
         )
 
-run_gzdoom_button = ttk.Button(
+profile_button = ttk.Button(
         master  = frame_buttons,
-        text    = "Run gzdoom", 
-        command = lambda: run(window)
+        text    = 'Create profile', 
+        command = create_profile,
         )
 
-run_gzdoom_button.pack(side = tk.LEFT, expand = True, fill = tk.X, padx = 5)
+profile_button.pack(side = tk.LEFT, expand = True, fill = tk.X, padx = 5)
 
 exit_button = ttk.Button(
         master  = frame_buttons,
@@ -187,9 +235,10 @@ exit_button = ttk.Button(
 
 exit_button.pack(side = tk.RIGHT, expand = True, fill = tk.X, padx = 5)
 
-frame_wads.pack(pady = 2, fill = tk.BOTH, expand = True
+frame_profile.pack(fill = tk.X, pady = 5)
+frame_wads.pack(pady = 5, fill = tk.BOTH, expand = True
     )
-frame_config.pack(fill = tk.X)
-frame_buttons.pack(fill = tk.X, pady = 2)
+frame_config.pack(fill = tk.X, pady = 5)
+frame_buttons.pack(fill = tk.X, pady = 5)
 
 tk.mainloop()
